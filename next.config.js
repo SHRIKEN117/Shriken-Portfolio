53057b3b401ae341d553060193141499ce9fbbdf
Shriken. <shrikenpatel@gmail.com.>
2026-05-08 18:31:15 -0400

    fix: explicit webpack alias for @ path to fix Render Linux build

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   images: {
     unoptimized: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
   },
 };
 
